3049ccaa68c35e6aeaf8428cbe2250c967dbbd88
keziiia <keziaoktavioni@gmail.com>
2023-07-24 22:33:52 +0700

    login register

---
+++ app/src/main/java/com/example/vacash/Login.java
@@ -0,0 +1,15 @@
+package com.example.vacash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
